28fb9a142fd8c2f7819325d46da5c35add1d8d5d
javlocan <javlocan.dev@gmail.com>
2024-03-01 17:33:42 +0100

    Fran stuff

--- ewwctl/src/main.rs
+++ ewwctl/src/main.rs
@@ -5,7 +5,10 @@ use std::{
     io::Error,
     net::{Ipv4Addr, SocketAddrV4, UdpSocket},
     process::Command,
-    sync::{mpsc::sync_channel, Arc, Mutex},
+    sync::{
+        mpsc::{channel, sync_channel},
+        Arc, Mutex,
+    },
     thread,
     time::Duration,
 };
@@ -44,7 +47,8 @@ fn main() {
 }
 
 fn run_server() -> Result<(), Error> {
-    let (tx, rx) = sync_channel::<Action>(1);
+    let (tx, rx) = channel();
+    let (t_dbnc, r_dbnc) = channel();
     // let (tx, rx) = channel();
     let ty = tx.clone();
 
@@ -58,17 +62,34 @@ fn run_server() -> Result<(), Error> {
             let (num_bytes, _src_addr) = socket.recv_from(&mut buf)?;
             let msg = std::str::from_utf8(&buf[..num_bytes]).expect("Error converting to UTF-8");
             let action = Action::from_msg(msg);
-            let _ = ty.send(action);
+            if !action.debounce {
+                let _ = ty.send(action);
+            } else {
+                let _ = t_dbnc.send(action);
+            }
+        }
+    });
+    let debounce: Arc<Mutex<Option<Module>>> = Arc::new(Mutex::new(None));
+
+    // -------------------------------------------------------
+    // this is a debouncing thread
+    // ------------------------------------------------------
+
+    let tz = tx.clone();
+    thread::spawn(move || loop {
+        let action = r_dbnc.recv().unwrap();
+        match r_dbnc.recv_timeout(Duration::from_millis(300)) {
+            Ok(_) => {}
+            Err(_) => {
+                tz.send(action);
+            }
         }
     });
 
-    // let debounced: Arc<Mutex<Option<Module>>> = Arc::new(Mutex::new(None));
-    let debounce: Arc<Mutex<Option<Module>>> = Arc::new(Mutex::new(None));
     let hover: Arc<Mutex<Option<Module>>> = Arc::new(Mutex::new(None));
 
     loop {
         let action = rx.recv().unwrap();
-        let tz = tx.clone();
 
         let arg = &action.module.to_string();
 
@@ -81,37 +102,12 @@ fn run_server() -> Result<(), Error> {
 
         // MAYBE STATE with hovered and debounced
 
+        let tz = tx.clone();
         match action.event {
             Event::Hover => {
-                let debouncing = debouncing.lock().unwrap();
-                println!("debounced: {:?}", debouncing);
-
                 if !action.debounce {
-                    println!("eww open {}", arg);
-
-                    match *debouncing {
-                        // CAMBIAR ESTO
-                        None => {
-                            // let hovered = Arc::clone(&hover);
-                            let mut hovered = hover.lock().unwrap();
-                            *hovered = Some(action.module);
-                            let _ = Command::new("eww").arg("open").arg(arg).output().unwrap();
-                        }
-                        Some(Module::Volume) | Some(Module::Wifi) | Some(Module::Brightness) => {
-                            // if debounced.clone().unwrap() == action.module {
-                            //     *debounced = None;
-                            //     let _ = tz.send(Action {
-                            //         module: action.module,
-                            //         event: action.event,
-                            //         debounce: false,
-                            //     });
-                            // } else {
-                            //     *debounced = Some(action.module.clone());
-                            //     println!("i dont even know");
-                            //     let _ = tz.send(action);
-                            // }
-                        }
-                    }
+                    // CAMBIAR ESTO
+                    let _ = Command::new("eww").arg("open").arg(arg).output().unwrap();
 
                     // si viene debounced, no abre nunca ventana !! quitar Command::new
                 } else {
@@ -121,7 +117,12 @@ fn run_server() -> Result<(), Error> {
                             let debounced = Arc::clone(&debounce);
                             thread::spawn(move || {
                                 println!("debouncing");
-                                thread::sleep(Duration::from_millis(3000));
+                                ////////////////////////////////////////////////
+                                ////////////////////////////////////////////////
+                                ////////////////////////////////////////////////
+                                ////////////////////////////////////////////////
+                                // tengo un rx2
+                                thread::sleep(Duration::from_millis(2000));
                                 let mut debounced = debounced.lock().unwrap();
                                 *debounced = Some(action.module.clone());
                                 let _ = tz.send(action);
@@ -140,7 +141,11 @@ fn run_server() -> Result<(), Error> {
                             } else {
                                 Some(action.module.clone())
                             };
-                            let _ = tz.send(action);
+                            let _ = tz.send(Action {
+                                module: action.module,
+                                event: action.event,
+                                debounce: false,
+                            });
                         }
                     }
                 };
@@ -151,15 +156,15 @@ fn run_server() -> Result<(), Error> {
                 if debouncing.as_ref().unwrap() == &action.module {
                     *debouncing = None;
                 }
-                let mut hovered = hover.lock().unwrap();
-                let close = hovered.as_ref().unwrap_or_else(|| &action.module);
-                *hovered = None;
-                println!("eww close {}", close);
-                Command::new("eww")
-                    .arg("close")
-                    .arg(close.to_string())
-                    .output()
-                    .unwrap();
+                //     let mut hovered = hover.lock().unwrap();
+                //     let close = hovered.as_ref().unwrap_or_else(|| &action.module);
+                //     *hovered = None;
+                //     println!("eww close {}", close);
+                //     Command::new("eww")
+                //         .arg("close")
+                //         .arg(close.to_string())
+                //         .output()
+                //         .unwrap();
             }
         }
     }
